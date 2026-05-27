841c5a4a24ce8d8eddc959f760e2391329d32a95
salimkhan <salimkhannet9900@gmail.com>
2026-05-28 01:56:34 +0600

    fix webpack config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["lucide-react"],
   },
+  webpack: (config) => {
+    return config;
+  },
 };
 
 export default nextConfig;
